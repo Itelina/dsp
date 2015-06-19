e1d729eeb6d58a56aa63a1e417bfbb2cee051139
Itelina <itelinama@gmail.com>
2015-06-18 23:31:13 -0400

    Update strings.py

--- python/strings.py
+++ python/strings.py
@@ -64,20 +64,11 @@ def verbing(s):
 
 def not_bad(s):
     """
-    Given a string, find the first appearance of the substring 'not'
-    and 'bad'. If the 'bad' follows the 'not', replace the whole
-    'not'...'bad' substring with 'good'. Return the resulting string.
-    So 'This dinner is not that bad!' yields: 'This dinner is
-    good!'
-
-    >>> not_bad('This movie is not so bad')
-    'This movie is good'
-    >>> not_bad('This dinner is not that bad!')
-    'This dinner is good!'
-    >>> not_bad('This tea is not hot')
-    'This tea is not hot'
-    >>> not_bad("It's bad yet not")
-    "It's bad yet not"
+    import re
+    if re.search("not.*bad", s):
+       print s[0:re.search("not.*bad", s).start()] + "good" + s[(re.search("not.*bad", s).end()):]
+    else:
+       print s
     """
     raise NotImplementedError
 
