985b9b18f1b9f70be944f4fbf006c47274f3a293
Itelina <itelinama@gmail.com>
2015-06-18 23:37:47 -0400

    Update strings.py

--- python/strings.py
+++ python/strings.py
@@ -4,26 +4,59 @@
 
 def donuts(count):
     """
+    Given an int count of a number of donuts, return a string of the form 'Number of donuts: <count>', where <count> is the number passed in. However, if the count is 10 or more, then use the word 'many' instead of the actual count.
+
+    >>> donuts(4)
+    'Number of donuts: 4'
+    >>> donuts(9)
+    'Number of donuts: 9'
+    >>> donuts(10)
+    'Number of donuts: many'
+    >>> donuts(99)
+    'Number of donuts: many'
+    """
     if count < 10:
         print 'Number of donuts:' + str(int(count))
     else:
         print 'Number of donuts: many'
-    """
-    raise NotImplementedError
 
 
 def both_ends(s):
     """
+    Given a string s, return a string made of the first 2 and the last 2 chars of the original string, so 'spring' yields 'spng'. However, if the string length is less than 2, return instead the empty string.
+
+    >>> both_ends('spring')
+    'spng'
+    >>> both_ends('Hello')
+    'Helo'
+    >>> both_ends('a')
+    ''
+    >>> both_ends('xyz')
+    'xyyz'
+    """
+    
     if len(str(s)) >= 2:
         newstring = str(s)[:2] + str(s)[-2:]
         print newstring
     else:
         print ""
-    """
-    raise NotImplementedError
-
+    
 
 def fix_start(s):
+    """
+    Given a string s, return a string where all occurences of its
+    first char have been changed to '*', except do not change the
+    first char itself. e.g. 'babble' yields 'ba**le' Assume that the
+    string is length 1 or more.
+
+    >>> fix_start('babble')
+    'ba**le'
+    >>> fix_start('aardvark')
+    'a*rdv*rk'
+    >>> fix_start('google')
+    'goo*le'
+    >>> fix_start('donut')
+    'donut'
     """
     b = s[0]
     for i in range(1, len(s)):
@@ -32,13 +65,22 @@ def fix_start(s):
        else:
            b += s[i]
    print b
-   """
-    raise NotImplementedError
-
 
 def mix_up(a, b):
     """
-    def mix_up(a, b):
+    Given strings a and b, return a single string with a and b
+    separated by a space '<a> <b>', except swap the first 2 chars of
+    each string. Assume a and b are length 2 or more.
+
+    >>> mix_up('mix', 'pod')
+    'pox mid'
+    >>> mix_up('dog', 'dinner')
+    'dig donner'
+    >>> mix_up('gnash', 'sport')
+    'spash gnort'
+    >>> mix_up('pezzy', 'firm')
+    'fizzy perm'
+    """
     first = list(a)
     second = list(b)
     x = first[0:2] 
@@ -46,11 +88,20 @@ def mix_up(a, b):
     first[0:2] = y
     second[0:2] = x
     print ''.join(first) + ' ' + ''.join(second)
-    """
-    raise NotImplementedError
-
 
 def verbing(s):
+    """
+    Given a string, if its length is at least 3, add 'ing' to its end.
+    Unless it already ends in 'ing', in which case add 'ly' instead.
+    If the string length is less than 3, leave it unchanged. Return
+    the resulting string.
+
+    >>> verbing('hail')
+    'hailing'
+    >>> verbing('swiming')
+    'swimingly'
+    >>> verbing('do')
+    'do'
     """
     if len(s) >=3:
         if s[-3:] == 'ing':
@@ -58,20 +109,29 @@ def verbing(s):
         else:
             s += 'ing'
     print s
-    """
-    raise NotImplementedError
-
 
 def not_bad(s):
+    """
+    Given a string, find the first appearance of the substring 'not'
+    and 'bad'. If the 'bad' follows the 'not', replace the whole
+    'not'...'bad' substring with 'good'. Return the resulting string.
+    So 'This dinner is not that bad!' yields: 'This dinner is
+    good!'
+
+    >>> not_bad('This movie is not so bad')
+    'This movie is good'
+    >>> not_bad('This dinner is not that bad!')
+    'This dinner is good!'
+    >>> not_bad('This tea is not hot')
+    'This tea is not hot'
+    >>> not_bad("It's bad yet not")
+    "It's bad yet not"
     """
     import re
     if re.search("not.*bad", s):
        print s[0:re.search("not.*bad", s).start()] + "good" + s[(re.search("not.*bad", s).end()):]
     else:
        print s
-    """
-    raise NotImplementedError
-
 
 def front_back(a, b):
     """
